b7921ba70f33ef96d5ecc188118fae998324b3fa
Jonathan Argentiero <jonathan.argentiero@gmail.com>
2015-04-21 13:54:34 +0200

    wakSum first commit
    wakSum definition

---
+++ tests/wakSum-spec.js
@@ -0,0 +1,12 @@
+/* jshint ignore:start */
+describe("Testing wakSum", function() {
+
+	// check that wakSum is defined and a function
+	it("wakSum should be defined",function(){
+		expect(wakSum).to.be.a('function');
+	});
+
+    // ...
+
+});
+/* jshint ignore:end */
---
+++ wakMath.js
@@ -0,0 +1,13 @@
+// WAF.define('wakMath', ...)
+
+/**
+ * Sums two given numbers
+ * @param {a} number - The first addend
+ * @param {b} number - The second addend
+ * @method wakSum
+ */
+function wakSum(a,b){
+	
+}
+
+// ...
